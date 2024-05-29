ccc5cb2cd7c420f380025ef72b15e1f2bcb64ffc
Azzaxy <abdurrohmanazis@gmail.com>
2024-05-30 01:44:52 +0700

    add mock data for use data in searchLoker page

--- src/views/Worker/components/DetailLoker.jsx
+++ src/views/Worker/components/DetailLoker.jsx
@@ -2,6 +2,54 @@
 import React from "react";
 
 const DetailLoker = () => {
+  const loker = [
+    {
+      id: 1,
+      title: "Programmer",
+      company: "PT. ABC",
+      location: "Jakarta",
+      type: "Full Time",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      salary: "Rp. 1.000.000 - Rp. 2.000.000",
+      image: "https://via.placeholder.com/150",
+      date: "2024-01-01",
+      requirement: "lorem ipsum dolor sit amet",
+      benefit: "lorem ipsum dolor sit amet",
+      responsibility: "lorem ipsum dolor sit amet",
+    },
+    {
+      id: 2,
+      title: "Programmer",
+      company: "PT. ABC",
+      location: "Jakarta",
+      type: "Full Time",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      salary: "Rp. 1.000.000 - Rp. 2.000.000",
+      image: "https://via.placeholder.com/150",
+      date: "2024-01-01",
+      requirement: "lorem ipsum dolor sit amet",
+      benefit: "lorem ipsum dolor sit amet",
+      responsibility: "lorem ipsum dolor sit amet",
+    },
+    {
+      id: 3,
+      title: "Programmer",
+      company: "PT. ABC",
+      location: "Jakarta",
+      type: "Full Time",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      salary: "Rp. 1.000.000 - Rp. 2.000.000",
+      image: "https://via.placeholder.com/150",
+      date: "2024-01-01",
+      requirement: "lorem ipsum dolor sit amet",
+      benefit: "lorem ipsum dolor sit amet",
+      responsibility: "lorem ipsum dolor sit amet",
+    },
+  ];
+
   return <div>DetailLoker</div>;
 };
 
--- src/views/Worker/components/ListLoker.jsx
+++ src/views/Worker/components/ListLoker.jsx
@@ -1,8 +1,56 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Card } from "@nextui-org/react";
 
 const ListLoker = () => {
-  return <div>ListLoker</div>;
+  const listLoker = [
+    {
+      id: 1,
+      title: "Frontend Developer",
+      company: "BRI",
+      image: "https://via.placeholder.com/150",
+      salary: "Rp. 1.000.000 - Rp. 2.000.000",
+      location: "Jakarta",
+      type: "Full-time",
+      date: "2024-01-01",
+    },
+    {
+      id: 2,
+      title: "Backend Developer",
+      company: "BRI",
+      image: "https://via.placeholder.com/150",
+      salary: "Rp. 1.000.000 - Rp. 2.000.000",
+      location: "Jakarta",
+      type: "Full-time",
+      date: "2024-01-01",
+    },
+    {
+      id: 3,
+      title: "Fullstack Developer",
+      company: "BRI",
+      image: "https://via.placeholder.com/150",
+      salary: "Rp. 1.000.000 - Rp. 2.000.000",
+      location: "Jakarta",
+      type: "Full-time",
+      date: "2024-01-01",
+    },
+    {
+      id: 4,
+      title: "Mobile Developer",
+      company: "BRI",
+      image: "https://via.placeholder.com/150",
+      salary: "Rp. 1.000.000 - Rp. 2.000.000",
+      location: "Jakarta",
+      type: "Full-time",
+      date: "2024-01-01",
+    },
+  ];
+
+  return (
+    <section>
+      <Card></Card>
+    </section>
+  );
 };
 
 export default ListLoker;
