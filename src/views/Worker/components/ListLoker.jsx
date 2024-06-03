e5bab4dddc5de7edfab7190dea64ac85154bc44b
Azzaxy <abdurrohmanazis@gmail.com>
2024-06-03 21:36:33 +0700

    fix: remove comment code in list loker

--- src/views/Worker/components/ListLoker.jsx
+++ src/views/Worker/components/ListLoker.jsx
@@ -1,12 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Image,
-} from "@nextui-org/react";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 import { MdOutlineWatchLater } from "react-icons/md";
 
 import Company1 from "@/assets/landing/company/company1.svg";
@@ -17,13 +11,6 @@ import Company5 from "@/assets/landing/company/company5.svg";
 import Company6 from "@/assets/landing/company/company6.svg";
 import Company7 from "@/assets/landing/company/company7.svg";
 import Company8 from "@/assets/landing/company/company8.svg";
-// import Company9 from "@/assets/landing/company/company9.svg";
-// import Company10 from "@/assets/landing/company/company10.svg";
-// import Company11 from "@/assets/landing/company/company11.svg";
-// import Company12 from "@/assets/landing/company/company12.svg";
-// import Company13 from "@/assets/landing/company/company13.svg";
-// import Company14 from "@/assets/landing/company/company14.svg";
-// import Company15 from "@/assets/landing/company/company15.svg";
 
 const ListLoker = () => {
   const listLoker = [
