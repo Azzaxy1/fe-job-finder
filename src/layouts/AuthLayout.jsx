11790790e0dddde6738c29ff2e3d5369966dd983
Azzaxy <abdurrohmanazis@gmail.com>
2024-06-05 18:44:47 +0700

    refactor(auth) : title in authlayout use Helmet

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -8,8 +8,18 @@ import Login from "@/assets/login.svg";
 import Register from "@/assets/register.svg";
 import Reset from "@/assets/reset-password.svg";
 import SetPassword from "@/assets/set-password.svg";
+import { Link, useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const AuthLayout = ({ children, type }) => {
+  const location = useLocation();
+
+  const pathname = location.pathname.split("/")[1];
+  const cleanPath = pathname.replace(/-/g, " ");
+
+  const capitalizedPath =
+    cleanPath.charAt(0).toUpperCase() + cleanPath.slice(1);
+
   const authImage =
     type === "login"
       ? Login
@@ -20,24 +30,39 @@ const AuthLayout = ({ children, type }) => {
       : Reset;
 
   return (
-    <section className="lg:grid lg:min-h-screen lg:grid-cols-12">
-      <aside className="items-center justify-center hidden h-24 md:flex bg-blue lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
-        <img
-          alt="auth image"
-          src={authImage}
-          width={100}
-          className="object-cover w-[80px] me-4 lg:w-[380px]"
-        />
-      </aside>
-      <div className="flex flex-col items-center justify-center px-8 py-8 bg-white lg:py-0 lg:order-first lg:col-span-5 xl:col-span-6">
-        <img
-          src={Logo}
-          alt="logo"
-          className="object-cover w-[100px] lg:w-[70px]"
-        />
-        {children}
-      </div>
-    </section>
+    <>
+      <Helmet>
+        <title>{capitalizedPath} | Job Finder</title>
+      </Helmet>
+      <section className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <aside className="items-center justify-center hidden h-24 md:flex bg-blue lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt="auth image"
+            src={authImage}
+            width={100}
+            className="object-cover w-[80px] me-4 lg:w-[380px]"
+          />
+        </aside>
+        <div className="flex flex-col items-center justify-center px-8 py-8 bg-white lg:py-0 lg:order-first lg:col-span-5 xl:col-span-6">
+          {type === "login" ? (
+            <Link to="/">
+              <img
+                src={Logo}
+                alt="logo"
+                className="object-cover w-[100px] lg:w-[70px]"
+              />
+            </Link>
+          ) : (
+            <img
+              src={Logo}
+              alt="logo"
+              className="object-cover w-[100px] lg:w-[70px]"
+            />
+          )}
+          {children}
+        </div>
+      </section>
+    </>
   );
 };
 
