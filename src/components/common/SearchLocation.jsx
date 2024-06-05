52c8eb9e9614bd7d87878e049b4c8d4c076a96fd
Azzaxy <abdurrohmanazis@gmail.com>
2024-06-06 03:21:04 +0700

    add aria-label

--- src/components/common/SearchLocation.jsx
+++ src/components/common/SearchLocation.jsx
@@ -6,6 +6,7 @@ import { IoLocationOutline } from "react-icons/io5";
 const SearchLocation = () => {
   return (
     <Autocomplete
+      aria-label="Lokasi"
       variant="bordered"
       placeholder="Lokasi"
       className="w-full border border-gray-400 rounded-xl "
