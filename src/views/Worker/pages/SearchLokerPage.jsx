905c0aaaa5f009db79ae53620aeee845902ce25b
Azzaxy <abdurrohmanazis@gmail.com>
2024-06-06 12:30:44 +0700

    fix: dummy data in listLoker

--- src/views/Worker/pages/SearchLokerPage.jsx
+++ src/views/Worker/pages/SearchLokerPage.jsx
@@ -16,7 +16,7 @@ const SearchLokerPage = () => {
         <main className="flex flex-col min-h-screen gap-8">
           <SearchLoker />
           <section className="flex flex-col px-10 pb-10 sm:flex-row 2xl:px-72">
-            <div className="flex flex-col p-2 max-h-[500px] md:max-h-[1050px] overflow-y-auto max-w-[400px] sm:min-w-[400px] scroll-smooth">
+            <div className="flex flex-col p-2 max-h-[500px] md:max-h-[1350px] overflow-y-auto max-w-[400px] sm:min-w-[400px] scroll-smooth">
               <ListLoker />
             </div>
             <DetailLoker />
