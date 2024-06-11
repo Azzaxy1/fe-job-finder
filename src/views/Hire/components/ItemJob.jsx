14eafd81adf58bb59ad312574d6d645ae72bfd3f
Azzaxy <abdurrohmanazis@gmail.com>
2024-06-12 04:17:57 +0700

    style: change card transition hover

--- src/views/Hire/components/ItemJob.jsx
+++ src/views/Hire/components/ItemJob.jsx
@@ -9,27 +9,29 @@ import { Link } from 'react-router-dom'
 const ItemJob = ({ job }) => {
   return (
     <section>
-      <Card radius='sm' className="w-full md:max-w-[450px]">
-        <CardHeader className="flex gap-3">
-          <div className="flex flex-col">
-            <h2 className="text-base font-medium sm:text-lg hover:underline">
-              <Link to={`/manage-jobs/${job.id}`}>{job.title}</Link>
-            </h2>
-            <p className="text-sm text-green-500 sm:text-base">{job.salary}</p>
-          </div>
-        </CardHeader>
-        <Divider/>
-        <CardBody>
-          <div>{parse(truncateDescription(job.description, 100))}</div>
-        </CardBody>
-        <Divider/>
-        <CardFooter>
-          <p className="text-sm  text-[#6b7280] px-5 w-full flex justify-end items-center gap-2">
-          <MdOutlineWatchLater />
-          {job.date}
-        </p>
-        </CardFooter>
-      </Card>
+      <Link to={`/manage-jobs/${job.id}`}>
+        <Card radius='sm' className="w-full md:max-w-[450px] 2xl:max-w-[510px] transition-transform ease-in-out duration-300 hover:scale-105 transform cursor-pointer">
+          <CardHeader className="flex gap-3">
+            <div className="flex flex-col">
+              <h2 className="text-base font-medium sm:text-lg">
+                {job.title}
+              </h2>
+              <p className="text-sm text-green-500 sm:text-base">{job.salary}</p>
+            </div>
+          </CardHeader>
+          <Divider/>
+          <CardBody>
+            <div>{parse(truncateDescription(job.description, 150))}</div>
+          </CardBody>
+          <Divider/>
+          <CardFooter>
+            <p className="text-sm  text-[#6b7280] px-5 w-full flex justify-end items-center gap-2">
+            <MdOutlineWatchLater />
+            {job.date}
+          </p>
+          </CardFooter>
+        </Card>
+      </Link>
     </section>
   )
 }
--- src/views/Hire/components/Listjob.jsx
+++ src/views/Hire/components/Listjob.jsx
@@ -5,7 +5,7 @@ import { Button, Pagination } from '@nextui-org/react'
 
 const Listjob = () => {
   const [currentPage, setCurrentPage] = useState(1)
-  const jobPerPage = 4
+  const jobPerPage = 6
 
   const totalPage = Math.ceil(listJobHire?.length / jobPerPage)
   const startIndex = (currentPage - 1) * jobPerPage
@@ -18,7 +18,7 @@ const Listjob = () => {
   return (
     <section className='flex flex-col items-start justify-start w-full gap-4 mb-5 -mt-2 '>
       <div className='flex flex-row items-center justify-between w-full'>
-        <h2 className='text-lg font-semibold md:text-xl'>Daftar Lowongan Pekerjaan</h2>
+        <h2 className='text-base font-semibold md:text-xl'>Daftar Lowongan Pekerjaan</h2>
         <Button className='font-semibold text-white bg-blue'>Tambah Pekerjaan</Button>
       </div>
       <article className='flex flex-row flex-wrap w-full gap-4'>
