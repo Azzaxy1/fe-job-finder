d04e3079947e561a29aa032a8fe8fd6c9c0ecd60
Azzaxy <abdurrohmanazis@gmail.com>
2024-06-15 20:23:21 +0700

    refactor: pagination in hire manage-jobs useMemo

--- src/views/Hire/components/Listjob.jsx
+++ src/views/Hire/components/Listjob.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import ItemJob from './ItemJob'
 import { listJobHire } from '@/utils/local-data'
 import { Button, Pagination } from '@nextui-org/react'
@@ -9,8 +9,12 @@ const Listjob = () => {
   const jobPerPage = 6
 
   const totalPage = Math.ceil(listJobHire?.length / jobPerPage)
-  const startIndex = (currentPage - 1) * jobPerPage
-  const currentJobs = listJobHire.slice(startIndex, startIndex + jobPerPage)
+
+  const items = useMemo(() => {
+    const start = (currentPage - 1) * jobPerPage
+    const end = start + jobPerPage
+    return listJobHire.slice(start, end)
+  })
 
   const handlePageChange = (page) => {
     setCurrentPage(page)
@@ -20,12 +24,12 @@ const Listjob = () => {
     <section className='flex flex-col items-start justify-start w-full gap-4 mb-5 -mt-2 '>
       <div className='flex flex-row items-center justify-between w-full'>
         <h2 className='text-base font-semibold md:text-xl'>Daftar Lowongan Pekerjaan</h2>
-        <Link to="create-job">
+        <Link to="/create-job">
           <Button className='font-semibold text-white bg-blue'>Tambah Pekerjaan</Button>
         </Link>
       </div>
       <article className='flex flex-row flex-wrap w-full gap-4'>
-        {currentJobs.map((job, index) => (
+        {items.map((job, index) => (
           <ItemJob key={index} job={job} />
         ))}
       </article>
