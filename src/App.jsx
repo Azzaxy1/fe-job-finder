9f0c9d97ac3131e95723113ecfa7eef370af5f94
Azzaxy <abdurrohmanazis@gmail.com>
2024-06-17 05:26:31 +0700

    feat: protected page base on role user

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import LoginPage from '@/views/Auth/pages/LoginPage'
 import RegisterPage from '@/views/Auth/pages/RegisterPage'
 import Landing from '@/views/Landing/pages/Landing'
@@ -21,6 +21,7 @@ import { Spinner } from '@nextui-org/react'
 const App = () => {
   const dispatch = useDispatch()
   const isLoading = useSelector((states) => states.isLoading)
+  const authUser = useSelector((states) => states.authUser)
 
   useEffect(() => {
     dispatch(asyncIsLoadingProccess())
@@ -36,28 +37,42 @@ const App = () => {
     )
   }
 
+  const isAuthorized = (allowedRoles) => {
+    if (!authUser || !allowedRoles.includes(authUser.role)) {
+      return <Navigate to="/" />
+    }
+    return null
+  }
+
   return (
     <Routes>
-      <Route path="/">
-        <Route index element={<Landing />} />
-        <Route path="jobs" element={<SearchLokerPage />} />
-        <Route path="tips" element={<TipsPage />} />
-        <Route path="tips/:id" element={<DetailTips />} />
-        <Route path="profile" element={<WorkerProfilePage />} />
-      </Route>
+      {isAuthorized(['hire']) && (
+        <>
+          <Route path="/">
+          <Route index element={<Landing />} />
+          <Route path="jobs" element={<SearchLokerPage />} />
+          <Route path="tips" element={<TipsPage />} />
+          <Route path="tips/:id" element={<DetailTips />} />
+          <Route path="profile" element={<WorkerProfilePage />} />
+          </Route>
+        </>
+      )}
 
       {/* Auth */}
       <Route path="login" element={<LoginPage />} />
       <Route path="register" element={<RegisterPage />} />
 
       {/* Hire */}
-      <Route path="hire-dashboard" element={<Dashboard />} />
-      <Route path='manage-applyer' element={<ManageApplyer/>} />
-      <Route path="manage-jobs" element={<ManageJobs />} />
-      <Route path="manage-jobs/:id" element={<DetailJobPage />} />
-      <Route path="hire-profile" element={<HireProfile />} />
-      <Route path='create-job' element={<CreateJobPage/>} />
-
+      {isAuthorized(['worker']) && (
+        <>
+          <Route path="hire-dashboard" element={<Dashboard />} />
+          <Route path='manage-applyer' element={<ManageApplyer/>} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="manage-jobs/:id" element={<DetailJobPage />} />
+          <Route path="hire-profile" element={<HireProfile />} />
+          <Route path='create-job' element={<CreateJobPage/>} />
+        </>
+      )}
       <Route path="*" element={<NoteFoundPage />} />
     </Routes>
   )
