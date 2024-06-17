ce374facd5b725943d727eb29a3f717c7388d3f3
Anuraaaa <anwarfauzan75@gmail.com>
2024-06-18 03:22:47 +0700

    feat(hire/dashboard): sync data from api and redux global state to dashboard

--- src/views/Hire/pages/Dashboard.jsx
+++ src/views/Hire/pages/Dashboard.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import HireLayout from '@/layouts/HireLayout'
 import ListChart from '../components/ListChart'
+import { useSelector } from 'react-redux'
 
 const Dashboard = () => {
+  const hire = useSelector((state) => state.hire)
   return (
     <HireLayout>
-      <ListChart />
+      <div className='flex gap-5 w-full flex-wrap'>
+        <ListChart title={'Pekerjaan'} data={hire}/>
+        <ListChart title={'Pelamar'} data={hire}/>
+      </div>
     </HireLayout>
   )
 }
